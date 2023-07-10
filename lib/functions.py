60c8703be384635ec87ded0f7b8c76e2e91e8dc9
Morgan Morales-Howard <mmoraleshoward@gmail.com>
2023-07-09 23:37:38 -0500

    complete lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+   print('Hello, programmer!')
+   return
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+    return
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+    return
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number/2
